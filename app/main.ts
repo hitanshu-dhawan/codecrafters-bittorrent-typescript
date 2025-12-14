70b382690da27904b71e7a785f97d7bbb1088396
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2025-12-14 13:32:33 +0530

    Decode bencoded strings

--- app/main.ts
+++ app/main.ts
@@ -22,14 +22,10 @@ const args = process.argv;
 const bencodedValue = args[3];
 
 if (args[2] === "decode") {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    console.error("Logs from your program will appear here!");
-
-    // TODO: Uncomment the code below to pass the first stage
-    // try {
-    //     const decoded = decodeBencode(bencodedValue);
-    //     console.log(JSON.stringify(decoded));
-    // } catch (error) {
-    //     console.error(error.message);
-    // }
+    try {
+        const decoded = decodeBencode(bencodedValue);
+        console.log(JSON.stringify(decoded));
+    } catch (error) {
+        console.error(error.message);
+    }
 }
